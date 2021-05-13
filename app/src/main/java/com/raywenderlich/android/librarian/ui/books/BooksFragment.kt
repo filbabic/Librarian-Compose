390028352312b7dc548a0db5a56dbc4b868a9e8b
Filip Babic <fbabic96@gmail.com>
2021-05-13 11:10:56 +0200

    02-learn-about-composable-functions

--- app/src/main/java/com/raywenderlich/android/librarian/ui/books/BooksFragment.kt
+++ app/src/main/java/com/raywenderlich/android/librarian/ui/books/BooksFragment.kt
@@ -38,10 +38,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
+import com.raywenderlich.android.librarian.R
 import com.raywenderlich.android.librarian.model.Book
 import com.raywenderlich.android.librarian.model.Genre
 import com.raywenderlich.android.librarian.model.relations.BookAndGenre
@@ -82,6 +90,9 @@ class BooksFragment : Fragment() {
   ): View {
     addBookContract
     return ComposeView(requireContext()).apply {
+      setContent {
+        BooksContent()
+      }
     }
   }
 
@@ -89,6 +100,28 @@ class BooksFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
     loadGenres()
     loadBooks()
+  }
+
+  @Composable
+  fun BooksContent() {
+    Scaffold(topBar = { BooksTopBar() },
+      floatingActionButton = { AddNewBook() }) {
+
+    }
+  }
+
+  @Composable
+  fun BooksTopBar() {
+    TopAppBar(title = { Text(stringResource(id = R.string.my_books_title)) },
+      backgroundColor = colorResource(id = R.color.colorPrimary),
+      contentColor = Color.White)
+  }
+
+  @Composable
+  fun AddNewBook() {
+    FloatingActionButton(content = {
+      Icon(imageVector = Icons.Filled.Add, contentDescription = "Add Book")
+    }, onClick = { showAddBook() })
   }
 
   fun loadGenres() {
